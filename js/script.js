d2741e87dfe67e9cf5180fc88dc33d9ea1caeb5f
“lpesce”57 <lpesce@umich.edu>
2025-06-25 14:34:34 -0500

    Update

--- js/script.js
+++ js/script.js
@@ -17,3 +17,23 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
   
+  // Lightbox for profile image
+const modal = document.getElementById("imgModal");
+const img = document.getElementById("profileImage");
+const modalImg = document.getElementById("modalImg");
+const closeBtn = document.querySelector(".close");
+
+img.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+};
+
+closeBtn.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
